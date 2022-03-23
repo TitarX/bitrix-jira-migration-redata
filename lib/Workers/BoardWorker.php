e4c3a2184ec6157a1f3b28da2ee798d835389f51
Dmitriy Ignatenko <titarx@gmail.com>
2022-03-23 12:46:30 +0400

    BoardWorker is ready

--- lib/Workers/BoardWorker.php
+++ lib/Workers/BoardWorker.php
@@ -3,6 +3,7 @@
 namespace Dev\PerfCode\JiraMigrationReData\Workers;
 
 use Dev\PerfCode\JiraMigrationReData\Models\BoardTable;
+use Dev\PerfCode\JiraMigrationReData\Helpers\LogHelper;
 
 class BoardWorker extends Worker
 {
@@ -12,9 +13,9 @@ class BoardWorker extends Worker
         for ($i = 0; !$isRowsEnds; $i++) {
             $dbResult = BoardTable::getList(
                 [
-                    'order' => ['DOMAIN' => 'asc'],
+                    'order' => ['MEMBER_ID' => 'ASC', 'JIRA_ID' => 'ASC'],
                     'filter' => ['EXPORTED' => 'N'],
-                    'select' => ['DOMAIN', 'EXPORTED', 'FIELDS'],
+                    'select' => ['MEMBER_ID', 'JIRA_ID', 'EXPORTED', 'FIELDS'],
                     'limit' => self::DB_SELECT_LIMIT,
                     'offset' => (self::DB_SELECT_LIMIT * $i)
                 ]
@@ -26,7 +27,28 @@ class BoardWorker extends Worker
             }
 
             while ($arrResult = $dbResult->fetch()) {
-                //
+                if (!empty($arrResult['FIELDS'])) {
+                    $newData = [];
+
+                    if (isset($arrResult['FIELDS']['location']['projectId'])) {
+                        $newData['PROJECT_ID'] = $arrResult['FIELDS']['location']['projectId'];
+                    }
+
+                    $newData['FIELDS'] = '';
+
+                    $primaryKey = ['MEMBER_ID' => $arrResult['MEMBER_ID'], 'JIRA_ID' => $arrResult['JIRA_ID']];
+
+                    try {
+                        $updateResult = BoardTable::update($primaryKey, $newData);
+                        if (!$updateResult->isSuccess()) {
+                            $errorMessages = $updateResult->getErrorMessages();
+                            LogHelper::updateResultFileLog('BoardErrors', print_r($errorMessages, true), print_r($newData, true), print_r($primaryKey, true));
+                        }
+                    } catch (\Exception $exception) {
+                        $exceptionMessage = $exception->getMessage();
+                        LogHelper::updateResultFileLog('BoardExceptions', $exceptionMessage, print_r($newData, true), print_r($primaryKey, true));
+                    }
+                }
             }
         }
     }
