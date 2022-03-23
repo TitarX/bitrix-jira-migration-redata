0fe6fdd5cba4b138c8353ce9a8a243748e3b8828
Dmitriy Ignatenko <titarx@gmail.com>
2022-03-23 11:37:43 +0400

    StageWorker is ready

--- lib/Workers/StageWorker.php
+++ lib/Workers/StageWorker.php
@@ -2,7 +2,9 @@
 
 namespace Dev\PerfCode\JiraMigrationReData\Workers;
 
+use Bitrix\Main\DB\Exception;
 use Dev\PerfCode\JiraMigrationReData\Models\StageTable;
+use Dev\PerfCode\JiraMigrationReData\Helpers\LogHelper;
 
 class StageWorker extends Worker
 {
@@ -12,9 +14,9 @@ class StageWorker extends Worker
         for ($i = 0; !$isRowsEnds; $i++) {
             $dbResult = StageTable::getList(
                 [
-                    'order' => ['DOMAIN' => 'asc'],
+                    'order' => ['MEMBER_ID' => 'ASC', 'JIRA_ID' => 'ASC', 'JIRA_PROJECT_ID' => 'ASC'],
                     'filter' => ['EXPORTED' => 'N'],
-                    'select' => ['DOMAIN', 'EXPORTED', 'FIELDS'],
+                    'select' => ['MEMBER_ID', 'JIRA_ID', 'JIRA_PROJECT_ID', 'EXPORTED', 'FIELDS', 'POSITION'],
                     'limit' => self::DB_SELECT_LIMIT,
                     'offset' => (self::DB_SELECT_LIMIT * $i)
                 ]
@@ -26,7 +28,33 @@ class StageWorker extends Worker
             }
 
             while ($arrResult = $dbResult->fetch()) {
-                //
+                if (!empty($arrResult['FIELDS'])) {
+                    $newData = [];
+
+                    $lastKey = array_key_last($arrResult['FIELDS']['columnConfig']['columns']);
+                    $stageType = 'WORK';
+                    if ($arrResult['POSITION'] == 0) {
+                        $stageType = 'START';
+                    } elseif ($arrResult['POSITION'] == $lastKey) {
+                        $stageType = 'FINISH';
+                    }
+
+                    $newData['STAGE_TYPE'] = $stageType;
+                    $newData['FIELDS'] = '';
+
+                    $primaryKey = ['MEMBER_ID' => $arrResult['MEMBER_ID'], 'JIRA_ID' => $arrResult['JIRA_ID'], 'JIRA_PROJECT_ID' => $arrResult['JIRA_PROJECT_ID']];
+
+                    try {
+                        $updateResult = StageTable::update($primaryKey, $newData);
+                        if (!$updateResult->isSuccess()) {
+                            $errorMessages = $updateResult->getErrorMessages();
+                            LogHelper::updateResultFileLog('StageErrors', print_r($errorMessages, true), print_r($newData, true), print_r($primaryKey, true));
+                        }
+                    } catch (Exception $exception) {
+                        $exceptionMessage = $exception->getMessage();
+                        LogHelper::updateResultFileLog('StageExceptions', $exceptionMessage, print_r($newData, true), print_r($primaryKey, true));
+                    }
+                }
             }
         }
     }
